be1074d5924f27b0f60aa5b2a673a67f7a5d7279
Arlei Ferreira Farnetani Junior <farnetani@gmail.com>
2017-02-25 10:01:48 -0300

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './ext/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: './bundle.js'
+  }
+
+devServer: {
+  port:8080,
+  contentBase: './public',
+},
+
+module: {
+  loaders: [{
+    test: /.jsx?$/,
+    loader: 'babel-loader',
+    exclude: /node_modules/,
+    query: {
+      presets: ['es2015', 'react']
+    }
+  }]
+}
+
+}
